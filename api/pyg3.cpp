0fd08945b933b6299c005ce50648e10629ac4827
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2021-06-05 19:22:15 -0500

    cmp - !!!!!!!!!!!!!!!!!

--- api/pyg3.cpp
+++ api/pyg3.cpp
@@ -1,8 +1,12 @@
 #include <pybind11/pybind11.h>
 #include <domain/domain/Domain.h>
+#include <domain/node/Node.h>
+
+/* Error streams */
 #include <handler/OPS_Stream.h>
 #include <StandardStream.h>
 
+/* Create global error stream */
 StandardStream sserr;
 OPS_Stream *opserrPtr = &sserr;
 
@@ -10,5 +14,13 @@ namespace py = pybind11;
 
 PYBIND11_MODULE (pyg3, m)
 {
-    py::class_ < Domain > (m, "Domain").def (py::init ());
+    py::class_ < Domain > (m, "Domain")
+        .def (py::init ())
+        .def ("addNode", &Domain::addNode)
+        .def ("getNode", &Domain::getNode)
+    ;
+
+    py::class_ < Node > (m, "Node")
+        .def (py::init<int, int, float, float> ())
+    ;
 }
