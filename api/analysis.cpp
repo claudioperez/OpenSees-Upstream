fd18df20ca2442d9f9a04c95fd1dbc56832fa0aa
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2021-06-06 14:50:14 -0500

    cmp

--- api/analysis.cpp
+++ api/analysis.cpp
@@ -1,8 +1,12 @@
+#include "ops_stream.h"
+
 #include <StaticAnalysis.h>
 #include <AnalysisModel.h>
 #include <Linear.h>
 #include <PenaltyConstraintHandler.h>
 #include <RCM.h>
+
+#include <pybind11/pybind11.h>
 
 
 
